eb98dbe8957ea94f9b5385dc20e5e94d37493ab7
Trevor Watson <watsontrevor@gmail.com>
2022-08-05 15:48:38 -0600

    add contentful projects

--- pages/Dev.vue
+++ pages/Dev.vue
@@ -1,5 +1,49 @@
 <template>
   <b-container>
-    <h1>My Dev Projects</h1>
+    <div class="textCenter">
+      <h1>My Dev Projects</h1>
+    </div>
+    <b-card-group columns>
+      <b-card
+        v-for="(project, i) in projects"
+        :key="i"
+        :img-src="`${project.fields.mainImage.fields.file.url}`"
+        img-top
+      >
+        <b-card-text>
+          {{ project.fields.blurb }}
+        </b-card-text>
+        <div style="text-align: center">
+          <nuxt-link :to="`${project.fields.slug}`">
+            <b-button>See Project</b-button>
+          </nuxt-link>
+        </div>
+      </b-card>
+    </b-card-group>
   </b-container>
 </template>
+
+<script>
+import { createClient } from "~/plugins/contentful.js";
+const client = createClient();
+
+export default {
+  name: "dev page",
+
+  asyncData({ env }) {
+    return Promise.all([
+      client.getEntries({
+        content_type: "devProjects",
+      }),
+    ])
+      .then(([entries]) => {
+        return {
+          projects: entries.items,
+        };
+      })
+      .catch(console.error);
+  },
+};
+</script>
+
+<style></style>
---
+++ pages/_slug.vue
@@ -0,0 +1,87 @@
+<template>
+  <b-container>
+    <nuxt-link to="/dev">
+      <b-button>back</b-button>
+    </nuxt-link>
+    <div>
+      <h1>{{ fields.projectName }}</h1>
+      <p>{{ fields.description.content[0].content[0].value }}</p>
+      <div>
+        <strong>Tech Stack:</strong>
+        <div v-for="(stack, i) in fields.stack" :key="i">
+          <code>{{ stack }}</code>
+        </div>
+      </div>
+    </div>
+    <b-carousel
+      id="project-carousel"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide
+        v-for="(screenshot, i) in screenshots"
+        :key="i"
+        :img-src="`${screenshot.fields.file.url}`"
+      ></b-carousel-slide>
+    </b-carousel>
+    <!-- <b-card :title="`${fields.projectName}`" :img-src="`${mainImage.file.url}`">
+      <b-card-text>
+        {{ fields.description.content[0].content[0].value }}
+      </b-card-text>
+    </b-card> -->
+  </b-container>
+</template>
+
+<script>
+import { createClient } from "~/plugins/contentful.js";
+const client = createClient();
+
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null,
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    },
+  },
+
+  asyncData({ env, params }) {
+    return Promise.all([
+      client.getEntries({
+        content_type: "devProjects",
+      }),
+    ])
+      .then(([entries]) => {
+        const project = entries.items.find(
+          (x) => x.fields.slug === params.slug
+        );
+        const fields = project.fields;
+        const mainImage = project.fields.mainImage.fields;
+        const screenshots = project.fields.screenshots;
+        console.log(screenshots);
+
+        return {
+          project,
+          fields,
+          mainImage,
+          screenshots,
+        };
+      })
+      .catch(console.error);
+  },
+};
+</script>
