c7375de08ea601fbc3812a0300ad9bb1f814549d
Trevor Watson <watsontrevor@gmail.com>
2022-10-23 00:39:43 -0600

    fathom fixes

--- plugins/fathom.client.js
+++ plugins/fathom.client.js
@@ -4,6 +4,7 @@ import VueFathom from "@ubclaunchpad/vue-fathom";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueFathom, {
     siteID: "BNAXHQYJ",
+    disabled: false,
     settings: {
       spa: "history",
     },
